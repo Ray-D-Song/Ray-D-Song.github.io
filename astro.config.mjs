8f5817f3e1d9bcaf816d85b68330a67701eedd01
Ray <songraysmail@gmail.com>
2024-08-06 12:00:26 +0800

    chore: Update npm dependencies and integrate cloudflare adapter
    - Update npm dependencies to the latest stable versions
    - Add "@astrojs/cloudflare" dependency
    - Add "@types/jsdom" and "jsdom" dependencies
    - Integrate cloudflare adapter in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,25 @@
 import { defineConfig } from 'astro/config';
-import unocss from "@unocss/astro"
-import { presetIcons, presetUno, presetTypography } from 'unocss'
+import unocss from "@unocss/astro";
+import { presetIcons, presetUno, presetTypography } from 'unocss';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss({
-      injectReset: true,
-    })
-  ],
+  integrations: [unocss({
+    injectReset: true
+  })],
   site: 'https://ray-d-song.com',
   markdown: {
+    syntaxHighlight: 'shiki',
     shikiConfig: {
       wrap: true,
       themes: {
         light: 'rose-pine-dawn',
-        dark: 'tokyo-night',
+        dark: 'tokyo-night'
       }
     }
-  }
-})
+  },
+  output: "server",
+  adapter: cloudflare()
+});
--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,18 +1,22 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt()
 
 export async function GET(context) {
   const blogs = await getCollection('blog')
   return rss({
     title: `Ray-D-Song's Blog`,
     description: 'Just for fun.',
-    site: context.site,
-    items: blogs.map((blog) => ({
-      title: blog.data.title,
-      description: blog.body.slice(0, 100),
-      pubDate: blog.data.date,
-      link: `https://ray-d-song.com/blog/${blog.slug}`,
-      content: blog.render()
-    })),
+    site: 'https://ray-d-song.com',
+    items: blogs.map((item) => ({
+      title: item.title,
+      pubDate: item.data.date,
+      link: `https://ray-d-song.com/blog/${item.slug}`,
+      content: sanitizeHtml(parser.render(item.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+    }))
   })
 }
