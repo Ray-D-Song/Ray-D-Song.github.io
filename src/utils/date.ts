f0f0f24b22b8badb7727587060255c24eff24470
Ray-D-Song <songraysmail@gmail.com>
2024-08-14 12:45:49 +0800

    feat: card tag

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,14 @@
-function formatDate(dateString: string) {
+function formatDate(dateString: string, weekday: boolean = true): string {
   const date = new Date(dateString);
+  if (weekday) {
+    return date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+  }
   return date.toLocaleDateString('en-US', {
-    weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric',
