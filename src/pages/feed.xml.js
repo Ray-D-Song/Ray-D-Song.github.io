7ccb1e414f900d581a7370623a42ad54e5889c05
Ray-D-Song <songraysmail@gmail.com>
2025-02-22 12:32:21 +0800

    Add About page, RSS link, and Iconify icon packages

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,19 +7,16 @@ const parser = new MarkdownIt()
 
 export async function GET(context) {
   const blogs = await getCollection('blog')
-  
-  // 根据 URL 参数判断语言
-  const url = new URL(context.request.url)
-  const lang = url.searchParams.get('lang') || 'zh-cn'
-  
-  // 过滤语言并按时间排序
+
   const filteredBlogs = blogs
-    .filter(blog => blog.slug.startsWith(lang))
-    .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
-    
+    .sort(
+      (a, b) =>
+        new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
+    );
+
   return rss({
     title: `Ray-D-Song's Blog`,
-    description: lang === 'zh-cn' ? '技术博客' : 'Tech Blog',
+    description: '我正在和日常战斗',
     site: 'https://ray-d-song.com',
     items: filteredBlogs.map((item) => ({
       title: item.data.title,
--- src/utils/tag.ts
+++ src/utils/tag.ts
@@ -1,17 +1,11 @@
 import { getCollection } from 'astro:content'
 
-type Lang = 'en' | 'zh-cn' | '*'
-
-async function getTags(lang: string) {
-  return Array.from(new Set(
-    (await getCollection('blog'))
-      .filter(blog => {
-        if(lang === '*') return true
-        return blog.slug.startsWith(`${lang}`)
-      })
-      .map(blog => blog.data.tag)
-      .flat()
-  ))
+async function getTags() {
+  return Array.from(
+    new Set(
+      (await getCollection("blog")).map((blog) => blog.data.tag).flat(),
+    ),
+  );
 }
 
 export {
